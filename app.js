4bdd9bbd2c62486317e82d599be58e12a3ca22a6
zl95826 <zl95826@hotmail.com>
2020-01-02 23:11:07 -0800

    update

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ const app=express();//create an express application and stored it in a constant 
                     //always running express as a function
 app.get('/favicon.ico', (req, res) => res.status(204));
 app.use('/',(req,res,next)=>{
-    console.log('Runs!');
+    console.log('Runs!');//This always runs
     next();
 });
 app.use('/add-product',(req,res,next)=>{
     console.log('In the middleware');
-    res.send('<h1>Product Page</h1>');
+    res.send('<form action="/" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    //action attribute=the path/the url, to which the requests should be sent
    // res.write('<h1>Product Page</h1>');next();//Without next(), the request can't continue its journey, so it will never reach a place 
-    //where we might send a response
+    //where we might send a response, so use the method send
 });//use method allows us to add a middleware function
 //you pass a function to app.use, the function will be executed for every incoming request
 //and this function will receive three arguments,the request object, response object, and next
 //next is a function. It has to be executed to allow the request to travel on to the next middleware in line.
+
+//we need a route or a middleware that handles requests to product(the data submitted in add-product was sent to product page)
+app.use('/product',(req,res,next)=>{
+    console.log(res.body);
+    res.redirect('/');
+});
+
 app.use('/',(req,res,next)=>{
     console.log('In Another middleware');
     res.send('<h1>Hello from /</h1>');
