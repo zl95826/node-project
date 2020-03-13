37ba0c9c55807e683cb08459c5c36a627573b58c
zl95826 <zl95826@hotmail.com>
2020-03-12 23:38:22 -0700

    callback

--- controllers/products.js
+++ controllers/products.js
@@ -8,6 +8,8 @@ exports.postAddProduct=(req,res,next)=>{
     res.redirect('/');}
 
 exports.getProducts=(req,res,next)=>{
-    const products=Product.fetchAll();
-    res.render('shop',{prods:products,pageTitle:'Shop',path:'/',hasProducts:products.length>0,activeShop:true})
+    const products=Product.fetchAll(products=>{
+        res.render('shop',{prods:products,pageTitle:'Shop',path:'/',hasProducts:products.length>0,activeShop:true})
+    });
+    
 }
--- models/product.js
+++ models/product.js
@@ -1,12 +1,30 @@
-const products=[];
+const fs=require('fs');
+const path=require('path');
+//const products=[]; 
+//we can save our product to a file instead of an array, so we need to work with the file system and import fs
 module.exports=class Product {
     constructor(t) {
        this.title=t; 
     }
     save() {
-        products.push(this);//this refers to the object created based on the class
+        const p=path.join(path.dirname(process.mainModule.filename),'data','products.json');
+        fs.readFile(p,(err,contentFile)=>{//since this is a json file, so the content of file should be in json format
+          let products=[];  
+          if(!err) {//Just means "if there is no error, then run this block of code".
+            products=JSON.parse(contentFile);//parse data to JS object
+          }
+          products.push(this);
+          fs.writeFile(p,JSON.stringify(products),err=>{
+              if(err) throw err;
+              console.log(' write file')});
+        });
     }
-    static fetchAll() {
-        return products;
+    static fetchAll(callback) {
+        const p=path.join(path.dirname(process.mainModule.filename),'data','products.json');
+        fs.readFile(p,(err,contentFile)=>{
+          if(err) {
+           cb([]);
+          }
+        callback(JSON.parse(contentFile));});  
     }
 }
